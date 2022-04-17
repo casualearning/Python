988cebb74b8623b0eb3171a00bf1a766ede7a2c5
Zuliang Wu <zuliangwu@hotmail.com>
2022-04-17 14:26:30 -0600

    fixing error

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ print('field reference')
 print('1|2|3\n-----4|5|6\n-----7|8|9')
 
 print(field_list[0] + '|' + field_list[1] + '|' + field_list[2] +'\n-----\n'
-     + field_list[3] + '|' + field_list[4] + '|' + field_list[5] + '|' +'\n-----\n'
+     + field_list[3] + '|' + field_list[4] + '|' + field_list[5] +'\n-----\n'
      + field_list[6] + '|' + field_list[7] + '|' + field_list[8])
 
 function.player1()
